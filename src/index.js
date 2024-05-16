f5567e6bb7a3641150ddbd2a0d45a9ed50e17429
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2024-05-16 01:42:05 -0500

    Add cache busting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Cache busting old ServiceWorker
+caches.keys().then(function(names) {
+  for (let name of names)
+      caches.delete(name);
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
