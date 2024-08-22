6bc1cef6699e1c7d76a68c5e2de6a73a54a81df0
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2024-08-22 10:50:36 -0500

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import './index.css';
 import App from './App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhNVeZKzT430RG0jjp-knBoT7utGZ82Og",
+  authDomain: "habeeb-bio.firebaseapp.com",
+  projectId: "habeeb-bio",
+  storageBucket: "habeeb-bio.appspot.com",
+  messagingSenderId: "149772551726",
+  appId: "1:149772551726:web:bc0fcf8d4dd0a3703941bf",
+  measurementId: "G-YNBBB3V3B5"
+}
+
+const app = initializeApp(firebaseConfig)
+getAnalytics(app)
 
 // Cache busting old ServiceWorker
 if ('serviceWorker' in navigator) {
